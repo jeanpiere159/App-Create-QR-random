2d02eaee21559acbc91c7d9e20184e03f2d4177a
Jean Pipi <jeanpiere159@hotmail.com>
2025-02-15 19:30:17 -0500

    Qr random

---
+++ script.js
@@ -0,0 +1,20 @@
+function GenerateQr() {
+    let imgBox = document.getElementById("imgBox");
+    let qrImage = document.getElementById("qrImage");
+    let qrText = document.getElementById("qrText");
+
+    if (qrText.value.length > 0) {
+        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + encodeURIComponent(qrText.value);
+        imgBox.classList.add("show-img");
+    } else {
+        qrText.classList.add("error");
+        setTimeout(() => {
+            qrText.classList.remove("error");
+        }, 1000);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Asignar la función GenerateQr al botón
+    document.querySelector("button").addEventListener("click", GenerateQr);
+});
